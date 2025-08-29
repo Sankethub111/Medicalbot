aa4c02266f7e1557755e5ac4b40d7df6bf726d5c
Sanket Thakur <sanketthakur631@gmail.com>
2025-08-29 21:48:34 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "medical_chatbot",
+    version = "0.1.0",
+    author = "Sanket Thakur",
+    author_email = "sanketthakur631@gmail.com",
+    pakages = find_packages(),
+    install_requires = []
+)   
